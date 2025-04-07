1d64970436212c57cb8f7da112b1a60842dbb971
AnimeshA867 <animeshacharya867+github@gmail.com>
2025-04-07 09:26:56 +0545

    initialize project structure with essential files and components

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,97 @@
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <div className="site-footer__top">
+          <div className="site-footer__description">
+            <h6>
+              <span>Animesh Acharya</span>
+            </h6>
+            <p>
+              Welcome to our e-commerce platform. We offer a wide range of
+              products to cater to all your needs. Shop with us for the best
+              deals and quality service.
+            </p>
+            <ul className="site-footer__social-networks">
+              <li>
+                <a href="#">
+                  <i className="icon-facebook" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="icon-twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="icon-linkedin" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="icon-instagram" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="icon-youtube-play" />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="site-footer__links">
+            <ul>
+              <li>Shopping online</li>
+              <li>
+                <a href="#">Order Status</a>
+              </li>
+              <li>
+                <a href="#">Shipping and Delivery</a>
+              </li>
+              <li>
+                <a href="#">Returns</a>
+              </li>
+              <li>
+                <a href="#">Payment options</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+            <ul>
+              <li>Information</li>
+              <li>
+                <a href="#">Gift Cards</a>
+              </li>
+              <li>
+                <a href="#">Find a store</a>
+              </li>
+              <li>
+                <a href="#">Newsletter</a>
+              </li>
+              <li>
+                <a href="#">Become a member</a>
+              </li>
+              <li>
+                <a href="#">Site feedback</a>
+              </li>
+            </ul>
+            <ul>
+              <li>Contact</li>
+              <li>
+                <a href="#">animeshachary867@gmail.com</a>
+              </li>
+              <li>
+                <a href="#">Hotline: +977 9876543210</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/index.tsx
@@ -0,0 +1,72 @@
+import Footer from "@/components/footer";
+import PageIntro from "@/components/page-intro";
+import ProductsFeatured from "@/components/products-featured";
+
+import Layout from "../layouts/Main";
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <PageIntro />
+
+      <section className="section">
+        <div className="container">
+          <header className="section__intro" style={{ marginTop: "80px" }}>
+            <h4>Why should you choose us?</h4>
+          </header>
+
+          <ul className="shop-data-items">
+            <li>
+              <i className="icon-shipping" />
+              <div className="data-item__content">
+                <h4>Free Shipping</h4>
+                <p>
+                  All purchases over $199 are eligible for free shipping via
+                  USPS First Class Mail.
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <i className="icon-payment" />
+              <div className="data-item__content">
+                <h4>Easy Payments</h4>
+                <p>
+                  All payments are processed instantly over a secure payment
+                  protocol.
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <i className="icon-cash" />
+              <div className="data-item__content">
+                <h4>Money-Back Guarantee</h4>
+                <p>
+                  If an item arrived damaged or you've changed your mind, you
+                  can send it back for a full refund.
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <i className="icon-materials" />
+              <div className="data-item__content">
+                <h4>Finest Quality</h4>
+                <p>
+                  Designed to last, each of our products has been crafted with
+                  the finest materials.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <ProductsFeatured />
+      <Footer />
+    </Layout>
+  );
+};
+
+export default IndexPage;
